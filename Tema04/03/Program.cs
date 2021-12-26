5fb7d563a5764e0cf46c40f4d4d8c56af43b575e
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2021-12-26 21:42:04 +0300

    Add files via upload

---
+++ Tema04/03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Zadanie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Тема 4. Задание 3
+            double a = 5, b = 4, P;
+            P = (a + b) / 2;
+            Console.WriteLine("           Тема 4. Задание 3");
+            Console.WriteLine($"Числа: {a}, {b}");
+            Console.WriteLine("Их среднее арифметическое: " + P);
+            Console.WriteLine("Нажмите любую кнопку");
+            Console.WriteLine();
+            Console.ReadKey();
+            Console.WriteLine();
+
+        }
+    }
+}
