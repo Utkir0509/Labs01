10e10d840f8b3a0992cfd89db3560e84dc45974b
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2021-12-26 21:41:34 +0300

    Add files via upload

---
+++ Tema04/02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zadanie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Тема 4. Задание 2
+            double d = 10, pi = 3.14, L;
+            L = d * pi;
+            Console.WriteLine("           Тема 4. Задание 2");
+            Console.WriteLine($"Диаметр окружности: {d}");
+            Console.WriteLine("Длина окружности: " + L);
+            Console.WriteLine("Нажмите любую кнопку");
+            Console.WriteLine();
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+    }
+}
