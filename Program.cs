ca9458ccc288ffb516237d9e84601ec95c051ac7
vova <vova@3R1>
2021-12-27 21:43:53 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3, b = 7;
+            Console.WriteLine($"Изначальные переменные: {a}, {b}");
+            b = a + b;
+            a = b - a;
+            b = b - a;
+            Console.WriteLine($"Переставленные переменные: {a}, {b}");
+        }
+    }
+}
