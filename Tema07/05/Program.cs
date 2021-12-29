5bf103f82cecd4a315a954c96ef57b22bb0761ca
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2021-12-29 18:18:41 +0300

    Add files via upload

---
+++ Tema07/05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tema7z5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = 50, B = 60, x ;
+            string str; //Строковая переменная для обработки ответа пользователя.
+
+            Console.WriteLine("Введите коэффицент A");
+            str = Console.ReadLine();
+            Double.TryParse(str, out A);
+
+            Console.WriteLine("Введите коэффицент B");
+            str = Console.ReadLine();
+            Double.TryParse(str, out B);
+
+            x = -B / A;
+
+            Console.WriteLine($"x={x}");
+        }
+    }
+}
