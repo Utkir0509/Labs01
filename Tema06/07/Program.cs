b04d21a100a3787e929bfc96536e8d5c90fa6e73
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2021-12-29 17:41:53 +0300

    Add files via upload

---
+++ Tema06/07/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _6._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x = 2, y, z;
+            Console.WriteLine($"a: {x}");
+            y = x;
+            z = y * y;
+            y = z * z;
+            z = y * y;
+             y = z * z;
+            x = y * (1 / x);
+            Console.WriteLine(x);
+            Console.WriteLine($"a^15: {x}");
+        }
+    }
+}
