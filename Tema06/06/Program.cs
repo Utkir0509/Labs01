7fb97a61a3d0df5f6b032c73dcb2f85f84def877
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2021-12-29 17:41:22 +0300

    Add files via upload

---
+++ Tema06/06/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tema6z6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=2, b;
+            Console.WriteLine($"a: {a}");
+            b = a * a;
+            a = b * b;
+            b = a * a;
+            Console.WriteLine($"b: {b}");
+        }
+    }
+}
