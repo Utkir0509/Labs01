f194da1d02a5df2cf8205e70e2ce92d52a9b07e6
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2021-12-29 18:14:25 +0300

    Add files via upload

---
+++ Tema07/03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace tema7z3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 2.0, y=3.0;
+            double a = 500, b, c;            
+            string str;
+            Console.WriteLine("Введите вес конфет");
+            str=Console.ReadLine();
+            Double.TryParse(str, out x);
+
+            Console.WriteLine("Введите стоимость этих конфет");
+            str = Console.ReadLine();
+            Double.TryParse(str, out a);
+            c = a / x; // Стоимость 1 кг конфет
+            Console.WriteLine("Введите интересующий вес конфет");
+            str = Console.ReadLine();
+            Double.TryParse(str, out y);
+
+            Console.WriteLine($"1 кг конфет стоит: {c} ");
+            b = c * y;// Стоимость y кг конфет
+            Console.WriteLine($"Конфеты весом {y} стоят: {b} ") ;
+        }
+    }
+}
