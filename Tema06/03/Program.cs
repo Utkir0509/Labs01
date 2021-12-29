4349e6380b0e6a9ed0ec5c2a4a1b8558be9bb768
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2021-12-29 17:36:52 +0300

    Add files via upload

---
+++ Tema06/03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace tema6z3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 1, B = 3, C = 5, X;
+            Console.WriteLine($"Значения переменных изначально: {A}, {B}, {C}");
+            X = C;
+            C = A;
+            A = B;
+            B = X;  
+            Console.WriteLine($"Новые значения переменных: A: {A}, B: {B}, C: {C}");
+        }
+    }
+}
