9f58a6a84b041ab7adec5e51360e28ebc4056c2d
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2021-12-29 18:02:21 +0300

    Add files via upload

---
+++ Tema07/02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace tema7z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a=3.14; //угол в радианах 
+            double g; //угол в градусах 
+            string str;
+            Console.WriteLine("Введите значения угла в радианах (меньше 2пи)");
+            str=Console.ReadLine();
+            double.TryParse (  str, out a);
+            g = a * 180 / Math.PI ;
+            Console.WriteLine($"Значение угла в {a} радиан является {g} градусов") ;
+        }
+    }
+}
