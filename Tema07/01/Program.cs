c8ecdc66ec7983bf7ae2dc3466fb6b5988bb5e59
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2021-12-29 17:48:05 +0300

    Add files via upload

---
+++ Tema07/01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace tema7z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 90;
+            double  r;
+            string str;
+            Console.WriteLine("Введите значение в градусах (целое число):");
+            str =Console.ReadLine();            
+            Int32.TryParse(str, out int b); 
+            r = b * Math.PI / 180;            
+            Console.WriteLine($"Значение угла в {str} градусов будет {r} радиан.");
+        }
+    }
+}
