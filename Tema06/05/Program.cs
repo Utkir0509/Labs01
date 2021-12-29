8cf5741c8e727a135175a4df68b1c66756ce054b
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2021-12-29 17:40:21 +0300

    Add files via upload

---
+++ Tema06/05/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace tema6z5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 5, y;
+            y = 4 * Math.Pow((x-3), 6) - 7 *Math.Pow((x-3),3) + 2 ;
+            Console.WriteLine($"x: {x}, y: {y}");
+        }
+    }
+}
