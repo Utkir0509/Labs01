27036deb87f2d06566e149432651f06778da4245
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-03 06:11:45 +0300

    Add files via upload

---
+++ Tema09/01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace tema9z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //С начала суток прошло N секунд (N — целое). Найти количество секунд, прошедших с начала последней минуты.
+            int m, s;
+            string str;
+            Console.WriteLine("Введите прошедшее количество секунд с начала суток");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int n); 
+
+            m = n / 60; //количество минут, прошедших с начала суток.
+            s = n - m * 60; //количество секунд, прошедших с начала последней минуты.
+            Console.WriteLine($"С начала суток прошло: {n} с, с последней минуты прошло {s} с.");
+        }
+    }
+}
