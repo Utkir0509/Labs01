92604c852bc3eca0488424c5e6f38dfd3eb0272a
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-03 07:01:57 +0300

    Add files via upload

---
+++ Tema04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Zadanie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ //Тема 4. Задание 1
+            double a=10, b=5, S, P;
+            S = a * b;
+            P = 2*(a + b);
+            Console.WriteLine("           Тема 4. Задание 1");
+            Console.WriteLine($"Стороны прямоугольника: {a}, {b}");
+            Console.WriteLine("Площадь прямоугольника: " + S);
+            Console.WriteLine("Периметр прямоугольника: " + P);
+            Console.WriteLine("Нажмите любую кнопку");
+            Console.WriteLine();
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+    }
+}
