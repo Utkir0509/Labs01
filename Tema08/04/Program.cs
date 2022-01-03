0e0b8c5f270276683dece3af2df9e558b2383bc2
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-03 06:04:37 +0300

    Add files via upload

---
+++ Tema08/04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tema8z4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дано двузначное число. Вывести число, полученное при перестановке цифр исходного числа.
+            int b; //результирующее число
+            int d, e; //Десятки, единицы
+            string str;
+            Console.WriteLine("Введите (целое число):");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int a);
+            
+            d = a / 10;
+            e = a % 10;
+            b = e * 10 + d;            
+            Console.WriteLine($"Исходное число: {a}, после перестановок: {b}");
+        }
+    }
+}
