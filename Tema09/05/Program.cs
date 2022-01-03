d0432ae9b966ca77ac9aae00699a5f49ce47884e
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-03 06:15:25 +0300

    Add files via upload

---
+++ Tema09/05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace tema9z5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Дан номер некоторого года (целое положительное число).
+             Определить соответствующий ему номер столетия, 
+            учитывая, что, к примеру, началом 20 столетия был 1901 год */
+            int s; // № столетия.
+            string str;
+            Console.WriteLine("Введите год:");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int y);
+            s = (y-1) / 100 + 1;
+            Console.WriteLine($"Год: {y} принадлежит {s}-му столетию.");
+        }
+    }
+}
