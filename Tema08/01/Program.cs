642ba8e276f4e41c36dd6b747e8ad33031f6e50e
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-03 05:57:44 +0300

    Add files via upload

---
+++ Tema08/01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tema8z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дан размер файла в байтах. Найти количество полных килобайтов, которые занимает данный файл
+            double sizeKBite;
+            string str;
+            Console.WriteLine("Введите размер файла в байтах (целое число):");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int sizeBite);
+
+            sizeKBite = (Math.Truncate(sizeBite / 1024.00));
+            Console.WriteLine($"Файл размером {sizeBite} байт, занимает полных {sizeKBite} КилоБайт.") ;
+        }
+    }
+}
