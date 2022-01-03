d39c25765edab3fc5dafa2e6235179b6d1f73608
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-03 06:08:40 +0300

    Add files via upload

---
+++ Tema08/05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace tema8z5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дано трехзначное число. В нем зачеркнули первую слева цифру и приписали ее справа. Вывести полученное число.
+            int r; //результирующее число
+            int a, b, c; //Сотни, десятки, единицы
+            string str;
+            Console.WriteLine("Введите трехзначное целое число:");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int i);
+
+            a = i / 100;
+            b = (i-a*100) / 10;
+            c = i % 10;
+            r = b * 100 + c*10+ a;
+            Console.WriteLine($"Исходное число: {i}, после перестановок: {r}");
+        }
+    }
+}
