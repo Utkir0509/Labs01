52008d04880e92481a7e879511ec39d5ac727df9
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-03 06:00:57 +0300

    Add files via upload

---
+++ Tema08/02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tema8z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Даны целые положительные числа A и B (A > B). На отрезке длины A размещено максимально возможное количество отрезков длины B (без наложений). Найти количество отрезков B, размещенных на отрезке A.
+            int n; //количество отрезков B, размещенных на отрезке A
+            string str;
+            Console.WriteLine("Введите размер отрезка А (целое положительное число):");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int A);
+            Console.WriteLine("Введите размер отрезка B (целое положительное число):");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int B);
+
+            n = A/B;
+            Console.WriteLine($"Количество отрезков размером: {B} , размещенных на отрезке A: {A} равно: {n}");
+        }
+    }
+}
