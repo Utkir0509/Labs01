7d738decd320858ad5b4b07ccc5c8cd9cf6d6ede
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-08 20:46:02 +0300

    Add files via upload

---
+++ Tema11/05/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace tema11z5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дано целое число.
+            //Вывести его строку-описание вида «отрицательное четное число»,
+            //«нулевое число», «положительное нечетное число» и т. д.
+
+            string str;
+            Console.WriteLine("Введите целое число");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int chislo);
+            str = "";
+            if (chislo == 0)
+            {
+                str = "нулевое число."; //проверяем на 0.
+                Console.WriteLine($"Число {chislo}: {str}.");
+                Environment.Exit(0);
+            }
+            if (chislo > 0)
+            {
+                str = "положительное ";
+            }
+            else if (chislo < 0)
+            {
+                str = "отрицательное ";
+            }
+
+            if (chislo % 2 == 0)
+            {
+                str += "четное ";
+            }
+            else
+            {
+                str += "нечетное ";
+            }
+            str += "число.";
+            Console.WriteLine($"Число {chislo}: {str}.");
+        }
+    }
+}
