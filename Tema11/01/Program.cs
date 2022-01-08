36059c7da6b8187cb49393b1e99962997220afb3
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-08 20:43:32 +0300

    Add files via upload

---
+++ Tema11/01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace tema11z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Даны две переменные целого типа: A и B.
+            //Если их значения не равны, то присвоить каждой переменной большее из этих значений,
+            //а если равны, то присвоить переменным нулевые значения.
+            //Вывести новые значения переменных A и B.
+            string str;
+
+            Console.WriteLine("Введите 1ое целое число");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int a);
+            Console.WriteLine("Введите 2ое целое число");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int b);
+
+            Console.WriteLine($"Изначальные числа a: {a}, b: {b}.");
+            if (a == b)
+            {
+                a = 0;
+                b = 0;
+            }
+            else
+            {
+                if (a > b) 
+                {
+                    b = a;
+                } 
+                else 
+                {
+                    a = b; 
+                }
+            }
+            Console.WriteLine($"Числа a: {a}, b: {b}.");
+        }
+    }
+}
