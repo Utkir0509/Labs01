4783fd3fa37a4ccfcda2d95fb5f7b7a841ead96a
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-08 20:41:27 +0300

    Add files via upload

---
+++ Tema10/02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace tema10z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Даны три целых числа: A, B, C.
+            //Проверить истинность высказывания:
+            //«Справедливо двойное неравенство A < B < C».
+            string str;
+            Boolean n;
+            Console.WriteLine("Введите целое число А:");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int a);
+            Console.WriteLine("Введите целое число B:");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int b);
+            Console.WriteLine("Введите целое число C:");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int c);            
+            
+            n = (a < b) & (b < c);
+            Console.WriteLine($"A < B < C: {n}");
+        }
+    }
+}
