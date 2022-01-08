836782015b22a77f28f9fd9ba0155cce0bcc4e62
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-08 20:45:47 +0300

    Add files via upload

---
+++ Tema11/04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace tema11z4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Даны координаты точки, не лежащей на координатных осях OX и OY.
+            //Определить номер координатной четверти,
+            //в которой находится данная точка
+            string str;
+
+            Console.WriteLine("Введите координату х точки");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int x);
+            Console.WriteLine("Введите координату y точки");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int y);
+
+            if ((x > 0) & (y > 0)) Console.WriteLine("Точка лежит в I координатной четверти");
+            if ((x < 0) & (y > 0)) Console.WriteLine("Точка лежит во II координатной четверти");
+            if ((x < 0) & (y < 0)) Console.WriteLine("Точка лежит в III координатной четверти");
+            if ((x > 0) & (y < 0)) Console.WriteLine("Точка лежит в IV координатной четверти");
+
+        }
+    }
+}
