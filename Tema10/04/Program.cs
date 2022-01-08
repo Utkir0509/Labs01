8c160d073fe799da55b189b4a538e4c86a1ca4ce
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-08 20:41:56 +0300

    Add files via upload

---
+++ Tema10/04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace tema10z4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дано трехзначное число. Проверить истинность высказывания:
+            //«Цифры данного числа образуют возрастающую
+            //или убывающую последовательность».
+            string str;
+            Boolean posledovVozr = false, posledovUbyv = false;
+            int a,b,c; // цифры из которых состоит число
+
+            Console.WriteLine("Введите целое трехзначное число");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int n);
+            
+            a = n / 100 ; //первая цифра числа.            
+            b = (n % 100) / 10; //вторая цифра числа.
+            c = (n % 100) % 10; //третья цифра числа.
+            if ((c > b) & (b >a)) posledovVozr = true; //проверяем что последовательность возрастающая.
+            if ((a > b) & (b > c)) posledovUbyv = true; //проверяем что последовательность убывающая.
+
+            //Console.WriteLine($"цифры:{a}, {b}, {c}");
+            Console.WriteLine($"Число : {n} состоит из цифр образущих последовательность возрастающую: {posledovVozr}, убывающую: {posledovUbyv}.");
+        }
+    }
+}
