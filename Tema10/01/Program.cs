6b978801b7e03bc935740b6d8baf77424bd5657d
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-08 20:41:08 +0300

    Add files via upload

---
+++ Tema10/01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace tema10z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Даны два целых числа: A, B. Проверить истинность высказывания: «Справедливы неравенства A > 2 и B ≤ 3»
+            
+            string str;
+            Console.WriteLine("Введите первое целое число А:");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int a);
+            Console.WriteLine("Введите второе целое число B:");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int b);
+
+            Console.WriteLine($"A > 2: {a > 2}, B <= 3:{b <= 3}");
+        }
+    }
+}
