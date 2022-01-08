7f58acb229e484930ce967fe48e808a271f298a4
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-08 20:46:18 +0300

    Add files via upload

---
+++ Tema11/06/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace tema11z6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дано целое число, лежащее в диапазоне 1–999.
+            //Вывести его строку описание вида «четное двузначное число»,
+            //«нечетное трехзначное число» и т. д.
+
+            string str;
+            Console.WriteLine("Введите целое число");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int chislo);
+
+            str = "";
+
+            if (chislo % 2 == 0)
+            {
+                str = "четное ";
+            }
+            else
+            {
+                str = "нечетное ";
+            }
+
+            if (chislo < 10)
+            {
+                str += "однозначное ";
+            }
+            else if (chislo < 100)
+            {
+                str += "двузначное ";
+            }
+            else if (chislo < 1000)
+            {
+                str += "трехзначное ";
+            }
+
+            str += "число.";
+            Console.WriteLine($"Число {chislo}: {str}.");
+
+        }
+    }
+}
