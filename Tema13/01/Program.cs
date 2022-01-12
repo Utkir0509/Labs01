4723e6e3ec0ee9cead28fe7cd2397fe7d945a9ca
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-12 19:32:00 +0300

    Add files via upload

---
+++ Tema13/01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tema13z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дано вещественное число — цена 1 кг конфет.
+            //Вывести стоимость 0.1, 0.2, . . . , 1 кг конфет.
+            string str;
+            
+            Console.WriteLine("Введите цену 1кг конфет (вещественное число):");
+            str = Console.ReadLine();
+            double.TryParse(str, out double price);
+            
+            for (double j = 0.1; j<=1.0; j+=0.1)
+            {
+                string result = string.Format("{0:f2}", j * price);
+                string ves = string.Format("{0:f1}", j);
+                Console.WriteLine($"Цена {ves} кг конфет: {result}");                
+            }            
+        }
+    }
+}
