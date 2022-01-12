44fd119ebba5652f971021a91f83a9c34a79abdb
Utkir0509 <94153229+Utkir0509@users.noreply.github.com>
2022-01-12 19:35:34 +0300

    Add files via upload

---
+++ Tema13/04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace tema13z4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дано вещественное число A и целое число N (> 0).
+            //Используя один цикл, найти сумму
+            //1 + A + A2 + A3 + . . . + AN
+            
+            double a, i; 
+            string str;
+
+            Console.WriteLine("Введите вещественное число A:");
+            str = Console.ReadLine();
+            Double.TryParse(str, out double A);
+            Console.WriteLine("Введите целое число N>0:");
+            str = Console.ReadLine();
+            Int32.TryParse(str, out int N);
+
+            a = 0;
+            for (i = 0; i <= N; i++)
+            {
+                a = a + Math.Pow(A,i);
+            }
+            Console.WriteLine($"A:{a}");
+        }
+    }
+}
